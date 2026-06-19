9cf2e2a827551c71cc8fda7a69e4698e93c4eedf
Kishoripatole <Kishoripatole2006@gmail.com>
2026-06-19 21:59:36 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,12 @@
+#single line comment
+print("Banana")
+a="Apple"
+b="Custard apple"
+print(a)
+print(b)
+"""print("Hello......")   multiline comments
+print("How are you.........!")
+print("whats going on!")"""
+
+C="Hyderabad"
+print(C)
