5d98bd82e1b9070c0118a6c1c1daf4bb4177dd33
Kishoripatole <Kishoripatole2006@gmail.com>
2026-06-27 22:36:15 +0530

    Add files via upload

---
+++ string/Negative_indexing.py
@@ -0,0 +1,15 @@
+C="Tulip flower"
+str_1=C[-1]
+print(str_1)
+
+str_2=C[-2]
+print(str_2)
+
+str_3=C[-3]
+print(str_3)
+
+str_4=C[-4]
+print(str_4)
+
+str_5=C[-5]
+print(str_5)
---
+++ string/slicing_of_string.py
@@ -0,0 +1,16 @@
+str="Daisy flower"
+
+str_1=str[:2]
+print(str_1)
+
+str_2=str[4:]
+print(str_2)
+
+str_3=str[1:8]
+print(str_3)
+
+str_4=str[:-5]
+print(str_4)
+
+str_5=str[:-6]
+print(str_5)
