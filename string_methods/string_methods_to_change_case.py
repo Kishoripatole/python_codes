ab1d5eb7158ae71cd7033b66e14673d7fdfbecad
Kishoripatole <Kishoripatole2006@gmail.com>
2026-06-27 17:09:43 +0000

    cases

---
+++ string_methods/string_methods_to_change_case.py
@@ -0,0 +1,16 @@
+str="hello python program"
+print("Upper case:",str.upper())
+print("Lower case:",str.lower())
+
+a="dahlia, tulip ,HELLO ,WORLD"
+
+print("Upper:",a.upper()) #converts all characters in the string to uppercase(Capital letters).
+
+print("Lower:",a.lower()) #onverts all characters in the string to lowercase(Small letters).
+
+print("Title:",a.title()) #capitalizes the first letter of each word.
+
+print("capitalize:",a.capitalize()) #capitalizes only the first letter of the string.
+
+print("Swapcase:",a.swapcase())  #swaps the uppercase letters in the string to the 
+                                  #lowercase; and vice versa.
