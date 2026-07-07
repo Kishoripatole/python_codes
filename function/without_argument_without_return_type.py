2a0fb56e2af8d3d56535ffce80971d3782768039
Kishoripatole <Kishoripatole2006@gmail.com>
2026-07-07 12:55:25 +0000

    7/7/26

---
+++ function/without_argument_without_return_type.py
@@ -0,0 +1,18 @@
+def printhello():
+    print("hello,world!")
+
+printhello()
+    
+def  sub():
+    a=int(input("enter first number:"))
+    b=int(input("enter second number:"))
+    print(a+b)
+
+sub()
+
+def add():
+    c=int(input("enter 1st number:"))
+    d=int(input("enter 2nd number:"))
+    print(c*d)
+add()
+
