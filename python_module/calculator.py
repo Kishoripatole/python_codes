09e46db53b552bd72f349828ce2c70b4eca4d24f
Kishoripatole <Kishoripatole2006@gmail.com>
2026-07-11 14:13:12 +0000

    11/7/26

---
+++ python_module/calculator.py
@@ -0,0 +1,22 @@
+def add(*n):
+      sum=0
+      for i in n:
+           sum+=i
+      return sum 
+
+def sub(a,b):
+      return a-b
+
+def multiplication(a,b):
+      return a*b
+
+def division(a,b):
+      return a/b
+
+def remainder(a=0,b=0):
+      return a%b
+
+def quotient(x,y):
+      return x/y
+
+
