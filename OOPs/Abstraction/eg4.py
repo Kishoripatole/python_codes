f884abff10e382b82755fafd3ef43c5399977e77
Kishoripatole <Kishoripatole2006@gmail.com>
2026-07-22 14:33:16 +0000

    22/7/26

--- OOPs/Abstraction/eg4.py
+++ OOPs/Abstraction/eg4.py
@@ -4,4 +4,10 @@ class flowers(ABC):
     def show(self):
         pass
     
-c
+class Tulip(flowers):
+    def show(self):
+         print("Tulip has very subtle,clean and airy fragrance!")
+
+
+t=Tulip()
+t.show()
