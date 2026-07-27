52ba097e9cc8b9d7d8be6a666c44685cfd25439c
Kishoripatole <Kishoripatole2006@gmail.com>
2026-07-27 14:38:33 +0000

    27/7/26

---
+++ Exception_handling/raise.py
@@ -0,0 +1,4 @@
+c=["Mumbai","Dubai","Hyderabad","Banglore"]
+
+if len(c)>3:
+    raise Exception("length of given list must be less than or equal to 5 but this is 5!")
---
+++ Exception_handling/try.py
@@ -0,0 +1,10 @@
+a=["Tulip","Dahlia","tulip and dahlia"]
+
+try:
+    for i in range(4):
+        print("index and element:",i,a[i])
+
+
+
+except:
+    print("Out of range!")
