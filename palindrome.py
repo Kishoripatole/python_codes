e6cc831af7d505e2afef0e3326370fc25aad42df
Kishoripatole <Kishoripatole2006@gmail.com>
2026-08-04 18:53:06 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+n=int(input("enter the number:"))
+n1=n//100
+n2=(n//10)%10
+n3=(n%100)%10
+rev=(n3*100)+(n2*10)+n1
+if n==rev:
+    print("Number is palindrome.")
+
+else:
+    print("Not palindrome .")
+ 
