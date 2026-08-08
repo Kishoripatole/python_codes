5d8bea6f9c68860fb961d4d0c1e657ac9e8b6c2b
Kishoripatole <Kishoripatole2006@gmail.com>
2026-08-08 17:45:12 +0000

    series

--- Pandas/Series.py
+++ Pandas/Series.py
@@ -1,21 +1,31 @@
-
+print("----printing series----")
 import pandas as pd
 a=[1,2,3,4,5]
 one=pd.Series(a)
 print(one)
+print("\n")
 
 #To access specified value
+print("print 2nd index")
 d=["monday","saturday","sunday"]
 e=pd.Series(d)
 print(d[2])
+print("\n")
 
 
 #create lables
+print("print with index")
 b=[10,20,30,40]
 two=pd.Series(b,index=["0","1","2","3"])
 print(two)
+print("\n")
 
 #Key/Value Objects as Series
+print("----dictionary----")
 c={"monday":1,"tuesday":2,"wednesday":3,"thursday":4}
 d=pd.Series(c)
 print(d)
+print("\n")
+
+e=pd.Series(c,index=["monday","tuesday"])
+print(e)
--- Pandas/p2.py
+++ Pandas/p2.py
@@ -1,7 +1,12 @@
+#
 import pandas as pd
 
-df=pd.read_csv("Pandas/teacher.csv")
+df=pd.read_csv("Pandas/people_100.csv")
 print(df)
 
+#to print first some lines
+print(df.head(10))
 
-print(df.head())
+#to print last some lines
+print(df.tail(2))
+
